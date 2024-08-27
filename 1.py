2c105e0b63c27c46daf2c9491e5d02bbe7cd3108
Arash <awrash.sn@gmail.com>
2024-08-27 12:31:37 +0430

    S1

---
+++ 1.py
@@ -0,0 +1,4 @@
+import phonenumbers
+
+for i in range(100):
+    phonenumbers.rightell()
---
+++ phonenumbers.py
@@ -0,0 +1,28 @@
+import random
+
+
+def irancell():
+    codes = ["0930", "0939", "0902", "0903"]
+    pn = f"{random.choice(codes)}"
+    for i in range(7):
+        pn += str(random.randint(0, 9))
+
+    print(pn)
+
+
+def hamrahaval():
+    codes = ["0911", "0912", "0910", "0919"]
+    pn = f"{random.choice(codes)}"
+    for i in range(7):
+        pn += str(random.randint(0, 9))
+
+    print(pn)
+
+
+def rightell():
+    codes = ["0921", "0922", "0923"]
+    pn = f"{random.choice(codes)}"
+    for i in range(7):
+        pn += str(random.randint(0, 9))
+
+    print(pn)
